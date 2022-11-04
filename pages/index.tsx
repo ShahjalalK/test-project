4ee3a38840d5ec83ffb169466eda0399f3c90d86
Shahjalal Khan <shahjalalkhan210@gmail.com>
2022-11-04 12:04:10 +0600

    dekha jak ki hoi

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,32 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import Link from 'next/link'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
  
   return (
     <>
-    <h1>Hello Shahjalal</h1>
-    <p>my name is shahjalal</p>
+       <div className="cards">
+       <div className="card">
+          <h1>Hello Shahjalal</h1>
+          <p>my name is shahjalal</p>
+          <Link href="http://www.facebook.com" target="_blank">Click Now</Link>
+        </div>
+
+        <div className="card">
+          <h1>Hello Shahjalal</h1>
+          <p>my name is shahjalal</p>
+          <Link href="http://www.facebook.com" target="_blank">Click Now</Link>
+        </div>
+
+        <div className="card">
+          <h1>Hello Shahjalal</h1>
+          <p>my name is shahjalal</p>
+          <Link href="http://www.facebook.com" target="_blank">Click Now</Link>
+        </div>
+
+       </div>
     
     </>
   )
